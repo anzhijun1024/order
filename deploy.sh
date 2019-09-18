66adfa12c3d3f5c6830a286c49381b273191b633
tangruiqiu <417770782@qq.com>
2019-09-18 17:04:29 +0800

    update  deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,25 @@
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
-export PROJ_PATH=/software/tomcat9/webapps/jenkins
+# export PROJ_PATH=/software/tomcat9/webapps/jenkins
 
 # 输入你的环境上tomcat的全路径
-export TOMCAT_APP_PATH=/software/tomcat9/
+# export TOMCAT_APP_PATH=/software/tomcat9/
 
 ### base 函数
 killTomcat()
 {
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
-    if [ "$pid" = "" ]
-    then
-      echo "no tomcat pid alive"
-    else
-      kill -9 $pid
+    #pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    #echo "tomcat Id list :$pid"
+    #if [ "$pid" = "" ]
+    #then
+    #  echo "no tomcat pid alive"
+    #else
+     # kill -9 $pid
     fi
+    #上面注释的或者下面的
+    cd $TOMCAT_APP_PATH/bin
+    sh shutdown.s
 }
 cd $PROJ_PATH/order
 mvn clean install
